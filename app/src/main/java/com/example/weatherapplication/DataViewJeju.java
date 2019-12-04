2a5d3150fe3b813c92b034878652550eb1e43aef
eastclo <goldeastcloud@gmail.com>
2019-12-05 04:27:09 +0900

    대기환경정보 파싱한 데이터 띄우기 및 배경색 변경 자바코드작성(제주) DataViewJeju.java

--- app/src/main/java/com/example/weatherapplication/DataViewJeju.java
+++ app/src/main/java/com/example/weatherapplication/DataViewJeju.java
@@ -1,14 +1,81 @@
 package com.example.weatherapplication;
 
-import android.os.Bundle;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.Pair;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.Iterator;
 
-public class DataViewJeju extends AppCompatActivity {
+public class DataViewJeju extends RelativeLayout {
+    //data set
+    public final static String fineDust = "FINE_DUST", ultraFineDust = "ULTRA_FINE_DUST", ozone = "OZONE";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_data_view_jeju);
+    TextView Jeju, Seogwipo;
+
+    public DataViewJeju(Context context, View view) {
+        super(context);
+        this.Jeju = view.findViewById(R.id.data_Jeju_jeju);
+        this.Seogwipo = view.findViewById(R.id.data_Jeju_seogwipo);
+    }
+    /*finedust: ~30 좋음, ~80 보통, ~150 나쁨
+     * ultrafinedust: ~15 좋음, ~35 보통, ~75 나쁨
+     *  yellowdust: ~0.030 좋음, ~0.090 보통, ~0.15 나쁨
+     * * */
+    public void setBackground(TextView view, String dataSet, String data){
+        Drawable drawable = null;
+        switch(dataSet){
+            case fineDust:
+                drawable = setColor(30,80,150,data);
+                break;
+            case ultraFineDust:
+                drawable = setColor(15,35,75,data);
+                break;
+            case ozone:
+                drawable = setColor(0.03,0.09,0.15, data);
+                break;
+        }
+        view.setBackground(drawable);
+    }
+
+    public Drawable setColor(double blue, double green, double orange, String data){
+        Drawable drawable;
+        if(data == "-"){
+            drawable = getResources().getDrawable(R.drawable.edge_radius_gray);
+            return drawable;
+        }
+        double value = Double.parseDouble(data);
+        if(value <= blue)
+            drawable = getResources().getDrawable(R.drawable.edge_radius_blue);
+        else if(value <= green)
+            drawable = getResources().getDrawable(R.drawable.edge_radius_green);
+        else if(value <= orange)
+            drawable = getResources().getDrawable(R.drawable.edge_radius_orange);
+        else
+            drawable = getResources().getDrawable(R.drawable.edge_radius_red);
+        return drawable;
+    }
+
+    public void setData(ArrayList<Pair<String,String>> dataList, String dataSet){
+        //String: 도시, String: 데이터값, dataSet: 통계정보 탭
+        Iterator<Pair<String,String>> it = dataList.iterator();
+        Pair<String,String> tmp;
+        Drawable background = null;
+        while(it.hasNext()) {
+            tmp = it.next();
+            switch (tmp.first) {
+                case "서귀포시":
+                    setBackground(Seogwipo, dataSet, tmp.second);
+                    Seogwipo.setText(tmp.second);
+                    break;
+                case "제주시":
+                    setBackground(Jeju, dataSet, tmp.second);
+                    Jeju.setText(tmp.second);
+                    break;
+            }
+        }
     }
 }
