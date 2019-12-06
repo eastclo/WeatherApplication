9d8a92ef171e0d9ccf058eba7a89d837e1328808
Ingyu Bang <zerfrawin@gmail.com>
2019-12-07 05:02:19 +0900

    실시간 날씨 투표
    파이어베이스 데이터 객체 단위 송수신 성공
    데이터 베이스 구조 변경 및 코드상 적용 완료
    데이터 베이스 가상 데이터 입력 완료 (파이어 베이스 - 파이어 스토어)
    송수신 기존 객체 변경 : 지역 총량을 저장
           추가 객체 생성 : 개인 투표 기록을 저장
    
    gps모듈 추가
    도-시,군 & 시-구,군 단위 구분 출력 완료

--- app/src/main/java/com/example/weatherapplication/RealTimeWeather.java
+++ app/src/main/java/com/example/weatherapplication/RealTimeWeather.java
@@ -1,27 +1,47 @@
 package com.example.weatherapplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
 
 public class RealTimeWeather extends AppCompatActivity implements View.OnClickListener {
+    // 버튼
     Button yesButton;
     Button noButton;
+    Spinner spinner;
     FirebaseFirestore db;
+    LocalBroadcastManager mLocalBroadcastManager;
+    BroadcastReceiver mReceiver;
+    //지역 정보 저장을 위한 변수 선언
+    String adminArea;
+    String locality;
+    List<Address> addresses = null;
+    TextView adminAreaView;
+    TextView loocalityView;
+    String uniqueID = UUID.randomUUID().toString();     // UUID 제대로 따옵시다
+    boolean didVoted;
+    boolean wasRight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,52 +50,110 @@ public class RealTimeWeather extends AppCompatActivity implements View.OnClickLi
 
         yesButton = findViewById(R.id.yesButton);
         noButton = findViewById(R.id.noButton);
+        db = FirebaseFirestore.getInstance();
+        spinner = findViewById(R.id.location_select);
+        adminAreaView = findViewById(R.id.admin_area);
+        loocalityView = findViewById(R.id.sub_locality);
+
         yesButton.setOnClickListener(this);
         noButton.setOnClickListener(this);
 
-        db = FirebaseFirestore.getInstance();
+        /*GPS 서비스 시작*/
+        Intent intent = new Intent(getApplicationContext(), GpsService.class);
+        startService(intent);
+        mLocalBroadcastManager = LocalBroadcastManager.getInstance(this);
+        mReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                double latitude = intent.getDoubleExtra(GpsService.EXTRA_LATITUDE, 0);
+                double longitude = intent.getDoubleExtra(GpsService.EXTRA_LONGITUDE, 0);
+                Geocoder coder = new Geocoder(getApplicationContext(), Locale.KOREA);
+                try {
+                    addresses = coder.getFromLocation(latitude, longitude, 3); // 첫번째 파라미터로 위도, 두번째로 경도, 세번째 파라미터로 리턴할 Address객체의 개수
+                    adminArea = addresses.get(0).getAdminArea();
+                    if (adminArea.charAt(adminArea.length()-1) == '시')
+                        locality = addresses.get(0).getSubLocality();
+                    else
+                        locality = addresses.get(0).getLocality();
+
+                    adminAreaView.setText(adminArea);
+                    loocalityView.setText((locality));
+
+                    //setSpinnerSelection();   //지역 정보에 따라 spinner default 값 설정을 위한 메소드 호출
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mLocalBroadcastManager.registerReceiver(mReceiver, new IntentFilter(GpsService.ACTION_LOCATION_BROADCAST));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopService(new Intent(this,GpsService.class));
     }
 
     @Override
     public void onClick(View view) {
-        String state = "stateTest";      // 지역 얻어오자
-        String city = "cityTest";
-
-//        DocumentReference docRef = db.collection(state).document(city);     // 객체 추출법 추가 요망
-//        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-//            @Override
-//            public void onSuccess(DocumentSnapshot documentSnapshot) {
-//                RealTimeVote current = documentSnapshot.toObject(RealTimeVote.class);
-//                cuurent.setYes(documentSnapshot.getString("yes"));
-//                cuurent.setNo(documentSnapshot.getString("no"));
-//            }
-//        });
-
-        int yes = 111;    // db에서 해당 지역의  true 추출
-        int no = 222;   // db에서 해당 지역의 false 추출
-
+        boolean myAnswer;
         if (view == yesButton)
-            yes++;
-        else    // noButton
-            no++;
+            myAnswer = true;
+        else
+            myAnswer = false;
+        applyVoteResult(myAnswer);
+    }
 
-        RealTimeVote vote = new RealTimeVote(yes, no);  // 추출한 data 가공후 첨가
-
-        db.collection("WeatherApplication").document("RealtimeWeather").collection(state)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d("sibal", document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            Log.d("sibal", "Error getting documents.", task.getException());
+    public void applyVoteResult(final boolean myAnswer) {
+        // 개인 투표 기록 검사
+        DocumentReference ownDocRef = db.collection("RealTimeWeather").document(adminArea).collection(locality).document(uniqueID);
+        ownDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                didVoted = true;
+                wasRight = true;
+                myAnswer answer = documentSnapshot.toObject(myAnswer.class);
+                if (answer == null) {   // 해당 유저가 해당 지역에 투표 하지 않은 경우
+                    answer = new myAnswer(myAnswer);
+                    db.collection("RealTimeWeather").document(adminArea).collection(locality).document(uniqueID).set(answer);
+                    didVoted = false;
+                } else {                   // 해당 유저가 해당 지역에 투표를 했던 경우
+                    if (answer.getAnswer() != myAnswer) {   // answer != myAnswer  =>  해당 유저가 이번에 투표한 내용이 이전 투표 내용과 달라졌을 경우
+                        answer.setAnswer(myAnswer);
+                        db.collection("RealTimeWeather").document(adminArea).collection(locality).document(uniqueID).set(answer);
+                        wasRight = false;
+                    }
+                }
+            }
+        });
+        //  해당 지역 투표 총량 조정
+        DocumentReference totalDocRef = db.collection("RealTimeWeather").document(adminArea).collection(locality).document("total");
+        totalDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                RealTimeVote total = documentSnapshot.toObject(RealTimeVote.class);
+                if (!didVoted) {   // 투표를 하지 않았던 경우, 이번 투표 내용을 +1
+                    if (myAnswer == true)
+                        total.setYes(total.getYes() + 1);
+                    else // myAnswer == false
+                        total.setNo(total.getNo() + 1);
+                } else {  // 투표를 했던 경우
+                    if (!wasRight) {  // 투표 기록이 바뀐 경우
+                        if (myAnswer == true) {
+                            total.setYes(total.getYes() + 1);
+                            total.setNo(total.getNo() - 1);
+                        } else { // myAnswer == false
+                            total.setNo(total.getNo() + 1);
+                            total.setYes(total.getYes() - 1);
                         }
                     }
-                });
-
-        db.collection("WeatherApplication").document("RealtimeWeather").collection(state).document(city).set(vote);
+                }
+                db.collection("RealTimeWeather").document(adminArea).collection(locality).document("total").set(total);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/weatherapplication/myAnswer.java
@@ -0,0 +1,20 @@
+package com.example.weatherapplication;
+
+public class myAnswer {
+    private boolean answer;
+
+    public myAnswer() {
+    }
+
+    public myAnswer(boolean answer) {
+        this.answer = answer;
+    }
+
+    public void setAnswer(boolean answer) {
+        this.answer = answer;
+    }
+
+    public boolean getAnswer() {
+        return this.answer;
+    }
+}
