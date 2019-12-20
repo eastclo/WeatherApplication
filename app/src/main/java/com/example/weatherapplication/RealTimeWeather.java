40ee7ca879a31c69cdf13c1e2353492c722cbd90
eastclo <goldeastcloud@gmail.com>
2019-12-20 19:39:43 +0900

    슬라이드 애니매이션 수정

--- app/src/main/java/com/example/weatherapplication/RealTimeWeather.java
+++ app/src/main/java/com/example/weatherapplication/RealTimeWeather.java
@@ -483,7 +483,7 @@ public class RealTimeWeather extends AppCompatActivity implements AdapterView.On
                 //터치 다운 X 위치에서 300픽셀을 초과 이동되면 애니매이션 실행
                 if (initialX - pointCurX > -300) {
                     startActivity(new Intent(this, MainActivity.class));
-                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+                    overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
                     finish();
                 }
         }
